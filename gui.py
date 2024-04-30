02762b4d93e31d80704cbe89688ba460bd4db5b7
zushii <zxgamingh@gmail.com>
2024-04-30 15:51:39 +0200

    brain rot

--- gui.py
+++ gui.py
@@ -1,25 +1,21 @@
 import tkinter as tk
 import random
 
+chosen_word = ""
+
 def guess_word():
+    global chosen_word
+
     with open("words.txt", "r", encoding="utf-8") as file:
         word = [line.strip() for line in file.readlines()]
         word_num = random.randint(0,len(word)-1)
         chosen_word = word[word_num]
-        return chosen_word
-def new_masked_word(a, b, c):
-    updated_masked_word = list(b)
-    for i in range(len(a)):
-        if a[i] == c:
-            updated_masked_word[i] = c
-    return "".join(updated_masked_word)
-def hangman(first_input):
-    print("hello world")
-   
 
+    print(chosen_word)
+    
 root = tk.Tk()
 root.title("hangman")
-root.geometry("900x700")
+root.geometry("700x500")
 
 title = tk.Label(root, text="Welcome, input a letter or word!")
 title.pack()
@@ -31,14 +27,76 @@ def get_input():
     guess = first_input.get()
     return guess
 
-first_input = get_input
+inc_letters = []
+inc_guesses = 0
 
-button = tk.Button(
+def inc_guess(a):
+    loss = 11
+    global inc_letters
+    global inc_guesses
+
+    inc_letters.append(a)
+    inc_guesses += 1
+    print(inc_letters)
+    print(inc_guesses)
+
+    if inc_guesses == loss:
+        print("you lost")
+
+def new_masked_word(correct, b, a):
+    updated_masked_word = list(a)
+    for i in range(len(correct)):
+        if correct[i] == b:
+            updated_masked_word[i] = b
+    masked_word = "".join(updated_masked_word)   
+
+def button_command(correct):
+    user_input = get_input().lower()
+
+    print(f"{masked_word} masked_word")
+
+    guessed_word = ""
+
+    if not user_input.isalpha():
+        print("[Invalid Input]")
+        print("Please try letters within the swedish alphabet")
+    elif user_input in inc_letters or user_input in masked_word:
+        print("[Invalid Input]")
+        print("You've already tried that letter")
+    elif len(user_input) != 1:
+        guessed_word = user_input
+        if guessed_word == correct:
+            print("win")
+        else:
+            print("inc")
+            
+    else:
+        if user_input in correct:
+            new_masked_word(correct, user_input, masked_word)
+            
+            print(masked_word)
+
+        else:
+            inc_guess(user_input)
+
+            print(inc_guesses)
+    print(inc_letters)
+
+guess_button = tk.Button(
     root,
     text="guess",
-    command=get_input
+    command=lambda: button_command(chosen_word)
+)
+ 
+guess_button.pack()
+
+next_button = tk.Button(
+    root,
+    text="next",
+    command=guess_word
+    
 )
 
-button.pack()
+next_button.pack()
 
 root.mainloop()
--- test.py
+++ test.py
@@ -1,30 +1,35 @@
 import tkinter as tk
-import random
 
+def reset_variable():
+    global variable
+    variable = "hello"
+    print("Variable reset to:", variable)
+
+num = 1
+def sum():
+    global num
+    num+=1
+
+print(num)
+sum()
+print(num)
+
+def function():
+    return "hello"
+
+variable = function()
+
+# Create the main application window
 root = tk.Tk()
-root.title("hangman")
-root.geometry("400x100")
+root.title("Reset Variable")
 
-title = tk.Label(root, text="Welcome, input a letter or word!")
-title.pack()
+# Create a label to display the current value of the variable
+label = tk.Label(root, text="Variable: " + variable)
+label.pack()
 
-first_input = tk.Entry(root)
-first_input.pack()
-
-def get_input():
-    guess = first_input.get()
-    return guess
-
-user_input = get_input()
-
-button = tk.Button(
-    root,
-    text="guess",
-    command=get_input
-)
-
+# Create a button to reset the variable
+button = tk.Button(root, text="Reset Variable", command=reset_variable)
 button.pack()
 
-print(user_input)
-
+# Run the Tkinter event loop
 root.mainloop()
